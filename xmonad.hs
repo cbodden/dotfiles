c59beacc6f903780803afcf83f36dce5bf528e15
cesar <cesar@pissedoffadmins.com>
2019-03-21 11:17:09 -0400

    changes: - changed tmux to lighten the load - changed xmonad.hs for xmobar support - added .xmobarrc conf file to track

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,11 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 --Simple variable declarations.
 myBorderWidth       = 1
@@ -20,14 +25,22 @@ myManageHook = composeAll
 tall = Tall 1 (3/100) (1/2)
 myLayoutHook = smartBorders tall ||| smartBorders (Mirror tall) ||| smartBorders Full
 
-main = xmonad $ def
+main = do
+  xmproc <- spawnPipe "xmobar"
+
+  xmonad $ defaultConfig
         { borderWidth        = myBorderWidth
         , modMask            = myModMask
         , terminal           = myTerminal
         , normalBorderColor  = "#D0D0D0"
         , focusedBorderColor = "#FFAA00"
-        , manageHook         = myManageHook
+        , manageHook         = manageDocks <+> manageHook defaultConfig
         , focusFollowsMouse  = myFocusFollowsMouse
-        , layoutHook         = myLayoutHook
-        , handleEventHook    = fullscreenEventHook
+        , layoutHook         = avoidStruts  $ layoutHook defaultConfig
+        , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
+        , logHook = dynamicLogWithPP xmobarPP
+                 { ppOutput = hPutStrLn xmproc
+                 , ppTitle = xmobarColor "green" "" . shorten 50
+                 , ppHiddenNoWindows = xmobarColor "grey" ""
+                 }
         }
