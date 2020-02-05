8d507dc42d665647e527c33ebcbb72870ff57c86
cesar <cesar@pissedoffadmins.com>
2020-02-04 22:33:37 -0500

    changes: - changing layout configs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,11 +4,18 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
+
+
+import XMonad hiding ( (|||) )
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.Grid
+import XMonad.Layout.SimpleFloat
+
 
 -- Simple variable declarations.
 myBorderWidth        = 0
@@ -34,8 +41,46 @@ myManageHook = composeAll
   ]
 
 -- Layouthook settings
-tall =  ResizableTall 1 (1/100) 1
-myLayoutHook = avoidStruts  $ layoutHook defaultConfig
+---- original
+--- tall =  ResizableTall 1 (1/100) 1
+--- myLayoutHook = avoidStruts  $ layoutHook defaultConfig
+
+--myLayoutHook = avoidStruts (tiled ||| Mirror tiled ||| Full ||| layoutHook defaultConfig)
+--  where
+--    tiled = spacing 3 $ Tall nmaster delta ratio
+--    nmaster = 1
+--    ratio = 1/2
+--    delta = 1/100
+
+
+
+myLayoutHook =
+    avoidStrutsOn [U] -- avoid statusbar overlapping
+        $ onWorkspace "web" floaT
+        $ onWorkspace "dev" tiled
+        $ standardLayouts
+    where
+        standardLayouts = tiled ||| mtiled ||| Grid ||| floaT
+        floaT   = simpleFloat
+        tiled1  = Tall nmaster delta ratio
+        mtiled  = Mirror tiled1
+        tiled   = spacing 5 $ ResizableTall nmaster delta ratio []
+        nmaster = 1       -- The default number of windows in the master pane
+        ratio   = 1/2     -- Default proportion of screen occupied by master pane
+        delta   = 3/100   -- Percent of screen to increment by when resizing panes
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- eventhook settings
 myEventHook = handleEventHook defaultConfig <+> docksEventHook
